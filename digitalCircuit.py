11768c8a1849a7e425ae7e3ba6370c9b0dafb43f
Wendy Dherin <wendydherin@gmail.com>
2014-12-12 14:02:09 -0800

    Add initial implemenation of digital circuit simulation.

---
+++ digitalCircuit.py
@@ -0,0 +1,53 @@
+class LogicGate:
+
+    def __init__(self,n):
+        self.label = n
+        self.output = None
+
+    def __str__(self):
+        return str(self.label)
+
+    def getLabel(self):
+        return str(self.label)
+
+    def getOutput(self):
+        self.output = self.performGateLogic()
+        return self.output
+
+class BinaryGate(LogicGate):
+
+    def __init__(self,n):
+        LogicGate.__init__(self,n)
+
+        self.pinA = None
+        self.pinB = None
+
+    def getPinA(self):
+        return int(input("Enter Pin A input for gate "+ self.getLabel()+"-->"))
+
+    def getPinB(self):
+        return int(input("Enter Pin B input for gate "+ self.getLabel()+"-->"))
+
+class UnaryGate(LogicGate):
+
+    def __init__(self,n):
+        LogicGate.__init__(self,n)
+
+        self.pin = None
+
+    def getPin(self):
+        return int(input("Enter Pin input for gate "+ self.getLabel()+"-->"))
+
+class AndGate(BinaryGate):
+
+    def __init__(self,n):
+        BinaryGate.__init__(self,n)
+
+    def performGateLogic(self):
+
+        a = self.getPinA()
+        b = self.getPinB()
+        if a==1 and b==1:
+            return 1
+        else:
+            return 0
