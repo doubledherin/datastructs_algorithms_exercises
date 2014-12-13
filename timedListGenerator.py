c2236d0d1b5960405bc40361c24c84083fdaeea7
Wendy Dherin <wendydherin@gmail.com>
2014-12-12 20:49:08 -0800

    Add empty function to timedListGenerator.py in order to see how long it takes just to call a function.

--- timedListGenerator.py
+++ timedListGenerator.py
@@ -16,6 +16,12 @@ def test3():
 def test4():
     l = list(range(1000))
 
+def test5():
+    """
+    Adding empty function to see how much time it takes just to call a function.
+    """
+    pass
+
 t1 = Timer("test1()", "from __main__ import test1")
 print ("concat ", t1.timeit(number=1000), "milliseconds")
 t2 = Timer("test2()", "from __main__ import test2")
@@ -24,3 +30,5 @@ t3 = Timer("test3()", "from __main__ import test3")
 print ("comprehension ", t3.timeit(number=1000), "milliseconds")
 t4 = Timer("test4()", "from __main__ import test4")
 print ("range ", t4.timeit(number=1000), "milliseconds")
+t5 = Timer("test5()", "from __main__ import test5")
+print ("empty ", t5.timeit(number=1000), "milliseconds")
