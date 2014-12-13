77c2e8ab3c35a8a308d2e698b9257065a5e47fe6
Wendy Dherin <wendydherin@gmail.com>
2014-12-12 21:12:49 -0800

    Modify popBenchmarker to test 10 increasingly large lists.

--- popBenchmarker.py
+++ popBenchmarker.py
@@ -8,4 +8,13 @@ x = list(range(2000000))
 print "popzero: ", popzero.timeit(number=1000)
 print "popend: ", popend.timeit(number=1000)
 
+print "pop(0)     pop()"
+for i in range (1000000, 10000001, 1000000):
+    x = list(range(i))
+    pe = popend.timeit(number=1000)
+    x = list(range(i))
+    pz = popzero.timeit(number=1000)
+    print "%15.5f, %15.5f" % (pz, pe)
+
+
 
