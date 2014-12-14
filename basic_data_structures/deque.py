0a700faac181bf55de2be377e624b02a43d51ab8
Wendy Dherin <wendydherin@gmail.com>
2014-12-13 18:40:48 -0800

    Add implementation of deque: deque.py.

---
+++ basic_data_structures/deque.py
@@ -0,0 +1,23 @@
+class Deque():
+
+    def __init__(self):
+        self.items[]
+
+    def addFront(item):
+        self.items.append(item)
+
+    def addRear(item):
+        self.items.insert(0, item)
+        
+    def removeFront():
+        return self.items.pop()
+
+    def removeRear():
+        return self.items.pop(0)
+
+    def isEmpty():
+        return self.items == []
+
+    def size():
+        return len(self.items)
+
