f2091eb1b260f6bc64ee52388c6aa28b412d154a
Wendy Dherin <wendydherin@gmail.com>
2014-12-16 21:37:42 -0800

    Small change to recursive function of baseConverter.py.

--- Chapter04_Recursion/baseConverter.py
+++ Chapter04_Recursion/baseConverter.py
@@ -1,6 +1,10 @@
-def baseConverter(num, base):
+from stack import Stack
+
+stacky = Stack()
+
+def baseConverter1(num, base):
     """
-    Takes an integer (base 10) and converts it to a string representation of that number in the indicated base (from 2 to 16, inclusive).
+    Using recursion, takes an integer (base 10) and converts it to a string representation of that number in the indicated base (from 2 to 16, inclusive).
     """
 
     digits = "0123456789ABCDEF"
@@ -8,7 +12,32 @@ def baseConverter(num, base):
     if num < base:
         return digits[num]
     else:
-        return baseConverter(num//2, base) + digits[num%base]
+        return baseConverter1(num//2, base) + digits[num%base]
+
+
+def baseConverter2(num, base):
+    """
+    Using a stack and recursion, takes an integer (base 10) and converts it to that number in the indicated base (from 2 to 16, inclusive).
+    """
+
+
+    digits = "0123456789ABCDEF"
+
+    if num < base:
+        stacky.push(digits[num])
+    else:
+        stacky.push(digits[num%base])
+        return baseConverter2(num//base, base) 
+
+    result = ""
+    while not stacky.isEmpty():
+        toAdd = stacky.pop()
+        result += toAdd
+    return result
+
+
+
 
 if __name__ == '__main__':
-    print baseConverter(10, 2)
+    # print baseConverter1(10, 2)
+    print baseConverter2(10, 2)
